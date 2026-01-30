4edf6ed133a6b5b2963f5760f96d447a850f3144
NaomiSoubhia <soubhiadoinaomi@gmail.com>
2026-01-30 14:49:28 -0500

    validate required form fields

--- assignments/labs/lab_three/contact.php
+++ assignments/labs/lab_three/contact.php
@@ -6,5 +6,28 @@ $lastname = filter_input(INPUT_POST, 'last_name', FILTER_SANITIZE_SPECIAL_CHARS)
 $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
 $message = filter_input(INPUT_POST, 'message', FILTER_SANITIZE_SPECIAL_CHARS);
 
+$errors = [];
+
+//require text fields
+if ($firstname === null || $firstname === '') {
+    $errors[] = "First name should not be empty or null";
+}
+
+if ($lastname === null || $lastname === '') {
+    $errors[] = "Last name should not be empty or null";
+}
+
+// email validation
+if ($email === null || $email === '') {
+    $errors[] = "Email should not be empty or null";
+} elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) { //validating the format 
+    $errors[] = "Email must be a valid format with @";
+}
+
+if ($message === null || $message === '') {
+    $errors[] = "Message should not be empty or null";
+}
+
+
 
 ?>
