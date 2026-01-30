a3f337e64c64ca90d845250c5bedf3cb62229dc9
NaomiSoubhia <soubhiadoinaomi@gmail.com>
2026-01-30 14:46:03 -0500

    start contact.php file

---
+++ assignments/labs/lab_three/contact.php
@@ -0,0 +1,10 @@
+<?php
+
+//collecting form data submitted via POST and sanitize inputs
+$firstname = filter_input(INPUT_POST, 'first_name', FILTER_SANITIZE_SPECIAL_CHARS);
+$lastname = filter_input(INPUT_POST, 'last_name', FILTER_SANITIZE_SPECIAL_CHARS);
+$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+$message = filter_input(INPUT_POST, 'message', FILTER_SANITIZE_SPECIAL_CHARS);
+
+
+?>
