9f900e2008e9b60fe439dfa74928c0edf1fb18a5
NaomiSoubhia <soubhiadoinaomi@gmail.com>
2026-02-06 12:24:59 -0500

    week 4

---
+++ 04/lab3/contact.php
@@ -0,0 +1,50 @@
+<?php
+
+//collecting form data submitted via POST and sanitize inputs
+$firstname = filter_input(INPUT_POST, 'first_name', FILTER_SANITIZE_SPECIAL_CHARS);
+$lastname = filter_input(INPUT_POST, 'last_name', FILTER_SANITIZE_SPECIAL_CHARS);
+$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+$message = filter_input(INPUT_POST, 'message', FILTER_SANITIZE_SPECIAL_CHARS);
+
+$errors = [];
+
+//require text fields
+if ($firstname === null || $firstname === '') {
+    $errors[] = "First name should not be empty or null";
+}
+
+if ($lastname === null || $lastname === '') {
+    $errors[] = "Last name should not be empty or null";
+}
+
+// email validation
+if ($email === null || $email === '') {
+    $errors[] = "Email should not be empty or null";
+} elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) { //validating the format 
+    $errors[] = "Email must be a valid format with @";
+}
+
+if ($message === null || $message === '') {
+    $errors[] = "Message should not be empty or null";
+}
+
+//echo errors
+if (!empty($errors)) {
+    foreach ($errors as $error) : ?>
+        <li><?php echo $error; ?></li>
+<?php endforeach;
+exit;
+}
+?>
+
+<main>
+
+    <?php echo "<h2>Thank for your message" . $firstname . "!</h2>" ?>
+
+    <h3>Message informations:</h3>
+    <?php echo "<p>Name: " . $firstname ." ". $lastname . "</p>" ?>
+    <?php echo "<p>Email: " . $email ."</p>" ?>
+    <?php echo "<p>Message: " . $message ."</p>" ?>
+</main>
+
+<?php require "includes/footer.php" ?>
---
+++ 04/lab3/index.php
@@ -0,0 +1,29 @@
+<?php require "includes/header.php" ?>
+<main>
+    <h2> Contact us!</h2>
+    <form action="contact.php" method="post">
+        <fieldset>
+            <legend>Contact Information</legend>
+            <label for="first_name">First name</label>
+            <input type="text" id="first_name" name="first_name" min="4" max="20" required>
+            <label for="last_name">Last name</label>
+            <input type="text" id="last_name" name="last_name" min="4" max="45" required>
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email" required>
+            <p>
+                <label for="message">Message:</label><br>
+                <textarea id="message" name="message" rows="6" placeholder="Type your message here..." maxlength="100"
+                    required></textarea>
+            </p>
+        </fieldset>
+
+        <button type="submit">Send</button>
+    </form>
+
+
+</main>
+</body>
+
+</html>
+
+<?php require "includes/footer.php" ?>
